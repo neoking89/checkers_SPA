123441aa294e725a84986226089b2a1f34522f88
neoking89 <v.ouwendijk@gmail.com>
2023-02-02 13:59:28 +0100

    update

--- static/script.js
+++ static/script.js
@@ -1,17 +1,40 @@
-const form = document.querySelector("#form");
+const nameForm = document.querySelector("#name-form");
+const moveForm = document.querySelector("#move-form");
+
 const whitePlayerInput = document.querySelector("#white-player");
 const blackPlayerInput = document.querySelector("#black-player");
-let playersList = document.querySelector("#players-list");
-const gameHeader = document.querySelector("#game-header");
-const gameBoard = document.querySelector("#game-board");
 
+const titleHeader = document.querySelector("#title-header");
+const game = document.querySelector("#game");
+// const gameHeader = document.querySelector("#game-header");
+// const gameBoard = document.querySelector("#game-board");
 
-const handleSubmit = (event) => {
+let playersList = [];
+let moveList = [];
+
+// 1. indien klik op name knop: stuur naam van players naar server
+// 2. indien 2 namen worden ontvangen: toggle de 2 views
+
+const nameSubmit = (event) => {
   event.preventDefault();
   const whitePlayer = whitePlayerInput.value;
   const blackPlayer = blackPlayerInput.value;
-  console.log(whitePlayer, blackPlayer);
+  if (!whitePlayer || !blackPlayer) {
+    // toon spelers error
+    return;
+  }
+  const result = postPlayers(whitePlayer, blackPlayer);
+};
 
+const showGame = (whitePlayer, blackPlayer) => {
+  game.hidden = false;
+  const gameHeader = game.querySelector("#game-header");
+  gameHeader.innerText = `Spel tussen ${whitePlayer} en ${blackPlayer}`;
+}
+
+
+
+function postPlayers(whitePlayer, blackPlayer) {
   fetch("http://localhost:3000/players", {
     method: "POST",
     headers: {
@@ -21,41 +44,60 @@ const handleSubmit = (event) => {
   })
     .then((response) => {
       if (!response.ok) {
-        throw new Error(`Network response was not ok, status: ${response.status}`);
+        throw new Error(
+          `Network response was not ok, status: ${response.status}`
+        );
       }
       return response.json();
     })
-    .then(() => {
-      playersList = addPlayersToList(playersList, whitePlayer, blackPlayer);
+    .then((data) => {
+      console.log("hallo", data);
+      const {whitePlayer, blackPlayer} = data;
+      playersList.push([whitePlayer, blackPlayer]);
+      console.log(playersList);
+      nameForm.hidden = true;
+      showGame(whitePlayer, blackPlayer);
     })
     .catch((error) => {
       console.error("There was a problem with fetch", error);
     });
-};
-
-function addPlayersToList(playersList, whitePlayer, blackPlayer){
-  const newListItem = document.createElement("li");
-  newListItem.innerText = `White Player: ${whitePlayer}, Black Player: ${blackPlayer}`;
-  playersList.appendChild(newListItem);
-  return playersList;
 }
 
-function showGame(whitePlayer, blackPlayer) {
-  gameHeader.hidden = false;
-  gameHeader.innerText = `Game between ${whitePlayer} and ${blackPlayer}`;
-  gameBoard.hidden = false;
-  form.hidden = true;
+function submitClicked(event) {
+  let messageDisplay = document.querySelector(".message-display");
+  messageDisplay.innerHTML = "";
+  let playerInput = document.querySelector(".player-text");
+  moveList.push(playerInput.value);
+  messageDisplay.innerHTML = "Last move was " + playerInput.value;
+  console.log(moveList);
 }
-
 
 // --------------------
-form.addEventListener("submit", handleSubmit);
-console.log(playersList);
-// showGame();
+nameForm.addEventListener("submit", nameSubmit);
 
 
 
 
 
 
-
+// function promptPlayerMove(player) {
+  //   moveForm.hidden = false;
+  //   const showPlayerToMove = document.querySelector("#show-player-to-move");
+  //   const text = document.createTextNode(`Zet van ${player}`);
+  //   showPlayerToMove.appendChild(text);
+  //   const moveInput = moveForm.querySelector("#move-input");
+  //   const moveSubmit = moveForm.querySelector("#move-submit");
+  
+  //   moveSubmit.addEventListener("click", (event) => {
+  //     event.preventDefault();
+  //     const move = moveInput.value;
+  //     // validatatie zet
+  //     if (!move) {
+  //       // toon zet error
+  //       return;
+  //     }
+  //     moveList.push(move);
+  //     // console.log(moveList);
+  //     // moveForm.hidden = true;
+  //   });
+  // }
